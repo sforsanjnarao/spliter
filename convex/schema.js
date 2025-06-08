0c0d8036e5814a9a6608449b09ec1d8591c66df2
sforsanjnarao <sanjnarao26@gmail.com>
2025-06-08 11:12:06 +0530

    Add schema definitions for expenses, groups, and settlements; implement protected routes in middleware; create Inngest API and client

--- convex/schema.js
+++ convex/schema.js
@@ -6,6 +6,59 @@ export default defineSchema({
         name:v.string(),
         email:v.string(),
         tokenIdentifier: v.string(),
-        imageUrl:v.string(),
-    }).index("by_token", ["tokenIdentifier"])
+        imageUrl:v.optional(v.string()),
+    })
+        .index("by_token", ["tokenIdentifier"])
+        .index("by_email",["email"])
+        .searchIndex("search_name", {searchField:"name"})
+        .searchIndex('search_email',{searchField:"email"}),
+
+    expenses:defineTable({
+        description:v.string(),
+        amount:v.number(),
+        category:v.optional(v.string()),
+        date:v.number(),
+        paidByUserId:v.id('users'),
+        splitType:v.string(),
+        splits:v.array(
+            v.object({
+                userId:v.id('users'),
+                amount:v.number(),
+                paid:v.boolean()
+            })
+        ),
+        createdBy:v.id("users")
+        .index("by_group",['groupId'])
+        .index("by_user_and_group",['paidByUserId','groupId'])
+        .index("by_date",['date'])
+
+
+    }),
+
+    groups:defineTable({
+        name:v.string(),
+        description:v.optional(v.string()),
+        createdBy:v.id("users"),
+        members:v.array(
+            v.object({
+                userId:v.id("users"),
+                role:v.string(),
+                joinedAt:v.number(),
+            })
+        )
+    }),
+    settlements:defineTable({
+        amount:v.number(),
+        note:v.optional(v.string()),
+        date:v.number(),
+        paidByUserId:v.id("users"),
+        receivedByUserId:v.id("users"),
+        relatedExpenseIds:v.optional(v.array(v.id("expenses"))),
+        groupId:v.optional(v.id("groups")),
+        createdBy:v.id("users"), 
+    })
+    .index("by_group",['groupId'])
+    .index("by_user_and_group",['paidByUserId','groupId'])
+    .index("by_recevier_and_group",['receivedByUserId','goupId'])
+    .index("by_date",['date'])
 })
--- middleware.js
+++ middleware.js
@@ -1,6 +1,24 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
+const isProtectedRoute=createRouteMatcher([
+    "/dashboard(.*)",
+    "/expenses(.*)",
+    "/contacts(.*)",
+    "/groups(.*)",
+    "/person(.*)",
+    "/settlements(.*)",
 
-export default clerkMiddleware()
+]);
+
+export default clerkMiddleware(async (auth,req)=>{
+    const {userId} =await auth();
+    if(!userId && isProtectedRoute(req)){
+        const {redirectToSignIn}=await auth();
+        return redirectToSignIn()
+    }
+
+    return NextResponse.next();
+})
 
 export const config = {
   matcher: [
