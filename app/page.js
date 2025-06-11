77b52e158af045c7a1f9424a5a4d1c042ad1ab85
sforsanjnarao <sanjnarao26@gmail.com>
2025-06-11 08:49:40 +0530

    Add landing section with call-to-action and footer; fix image path in testimonials

--- app/page.js
+++ app/page.js
@@ -168,6 +168,30 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <section className="gradient py-20">
+            <div className="container mx-auto px-4 md:px-6 text-center space-y-6">
+              <h2 className="text-3xl font-extrabold tracking-tight md:text-4xl text-white">
+              Ready to Simplify expen sharing?
+              </h2>
+              <p className="mx-auto max-w-[600px] text-green-100 md:text-xl/relaxed">
+                Join thousands of the users who have made splitting expenses
+                stress-free
+              </p>
+              <Button 
+              asChild
+              size={"lg"}
+              className="bg-green-800 hover:opacity-90"
+            >
+              <Link href={"/dashboard"}>
+                Get Started
+                <ArrowRight className="ml-2 h-4 w-4"/>
+              </Link>
+            </Button>
+            </div>
+      </section>
+      <footer className="border-t bg-gray-50 py-12 text-center text-sm text-muted-foreground">
+        Made by sanjana
+      </footer>
     </div>
   );
 }
